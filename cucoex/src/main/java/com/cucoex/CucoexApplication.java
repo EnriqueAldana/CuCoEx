c7f0546492d5d51de1f6f44a49258c664f229925
enrique <enrique@localhost>
2020-07-30 16:23:34 -0500

    Codigo Inicial

---
+++ cucoex/src/main/java/com/cucoex/CucoexApplication.java
@@ -0,0 +1,13 @@
+package com.cucoex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CucoexApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CucoexApplication.class, args);
+	}
+
+}
---
+++ cucoex/src/test/java/com/cucoex/CucoexApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cucoex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CucoexApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
