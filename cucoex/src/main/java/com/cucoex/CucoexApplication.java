dd968c67674ddf76fbfad32c3bffe25b0a314be7
enrique <enrique@localhost>
2020-07-30 17:25:31 -0500

    Segundo contacto

--- cucoex/src/main/java/com/cucoex/CucoexApplication.java
+++ cucoex/src/main/java/com/cucoex/CucoexApplication.java
@@ -7,7 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class CucoexApplication {
 
 	public static void main(String[] args) {
+		
 		SpringApplication.run(CucoexApplication.class, args);
+		
 	}
 
 }
